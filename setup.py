fe56b6114f23dac89f1cf2cf6ae17b992c68ce63
Jeremy Bridges <jbridges@live.com>
2024-06-19 15:19:51 -0400

    Committing first round of files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An over under project for MLB betting using python',
+    author='Jeremy Bridges',
+    license='',
+)
